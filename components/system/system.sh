e9ae8e6dbb74c7d886331f93ab5c6e1b25b88891
yevdeh <evgeniydegtyaryov@gmail.com>
2022-10-12 16:32:02 +0300

    system.sh: change gnome-tweak-tool to gnome-tweaks

--- components/system/system.sh
+++ components/system/system.sh
@@ -6,4 +6,4 @@ echo "snap" >> ~/.hidden
 gsettings set org.gnome.desktop.wm.preferences button-layout ''
 gsettings set org.gnome.shell favorite-apps "[]"
 
-sudo apt -y install chrome-gnome-shell gnome-tweak-tool
+sudo apt -y install chrome-gnome-shell gnome-tweaks
